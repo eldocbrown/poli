3e41b999d9817440319aa32cd79e9221a2a48cbe
Juan <juandonato@gmail.com>
2020-11-27 12:02:52 -0300

    Added loginfor policorp app, with supervisor validation

--- policorp/urls.py
+++ policorp/urls.py
@@ -5,10 +5,13 @@ from . import views
 app_name = "policorp"
 
 urlpatterns = [
+    path("", views.index, name="index"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
     path("tasks/", views.tasks, name="tasks"),
     path("availabilities/<int:taskid>/", views.availabilities, name="availabilities"),
     path("book/<int:availabilityid>/", views.book, name="book"),
     path("myschedule/", views.myschedule, name="myschedule"),
     path("cancelbooking/<int:bookingid>/", views.cancelbooking, name="cancelbooking"),
-    path("mysupervisedlocations/", views.mysupervisedlocations, name="mysupervisedlocations"),
+    path("mysupervisedlocations/", views.mysupervisedlocations, name="mysupervisedlocations")
 ]
--- policorp/views.py
+++ policorp/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
 from .models import Task, Availability, Booking, User
 
 # Create your views here.
+def index(request):
+    return render(request, "policorp/index.html");
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None and user.is_supervisor:
+            login(request, user)
+            return HttpResponseRedirect(reverse("policorp:index"))
+        else:
+            return render(request, "policorp/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "policorp/login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("policorp:index"))
+
 def tasks(request):
 
     # Only GET requests allowed
