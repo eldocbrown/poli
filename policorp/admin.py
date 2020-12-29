ca02da3b99e85a10246c4f617c47c559e4834ba0
Juan <juandonato@gmail.com>
2020-12-29 19:47:41 -0300

    Admin console list_display for Task, Location and User.

--- policorp/admin.py
+++ policorp/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from .models import *
 
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "duration")
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "is_supervisor")
+
 # Register your models here.
 
-admin.site.register(Location)
-admin.site.register(Task)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Task, TaskAdmin)
 admin.site.register(Availability)
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Booking)
