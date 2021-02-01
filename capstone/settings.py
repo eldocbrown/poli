07235a634eb556416c6b54c994a7564c39780c74
Juan Cruz <juandonato@gmail.com>
2021-02-01 17:24:34 -0300

    Added django-heroku package to settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +129,5 @@ USE_TZ = True
 STATIC_URL = os.path.join("/", "capstone", "static/")
 
 STATIC_ROOT = f"{BASE_DIR}{STATIC_URL}"
+
+django_heroku.settings(locals())
