5113470c9d4322e7fa06f10f50df9fe77addaf46
Juan Cruz <juandonato@gmail.com>
2021-04-10 16:00:47 -0300

    Environment settings

--- capstone/settings.py
+++ capstone/settings.py
@@ -25,12 +25,36 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.environ.get('DEBUG') == 'True' else False
-
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST_1')]
+# Database
+if IS_HEROKU_ENV:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    DEBUG = True if os.environ.get('DEBUG') == 'True' else False
+    ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST_1')]
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'poli',
+            'USER': 'poliuser',
+            'PASSWORD': os.environ.get('DATABASE_PG_PASSWORD'),
+            'HOST': os.environ.get('DATABASE_PG_HOST'),
+            'PORT': os.environ.get('DATABASE_PG_PORT')
+        }
+    }
+else:
+    SECRET_KEY = 'pauh61syjzkx6$1d%euial5h&y-1=rlr39_z5e&g^7%!en)c$x'
+    DEBUG = True
+    ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST_1'), '0.0.0.0', '127.0.0.1']
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'poli',
+            'USER': 'poliuser',
+            'PASSWORD': 'poliuser',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
+    }
 
 
 # Application definition
@@ -75,21 +99,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'capstone.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'poli',
-        'USER': 'poliuser',
-        'PASSWORD': os.environ.get('DATABASE_PG_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_PG_HOST'),
-        'PORT': os.environ.get('DATABASE_PG_PORT')
-    }
-}
 
 AUTH_USER_MODEL = 'policorp.User'
 
